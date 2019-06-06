15fbcebaa53d128de89cf1c11bd45d50543e9b0a
pramod.kharade <pramod.kharade@blazeclan.com>
2019-06-06 10:20:56 +0530

    find more than one record and count

--- mongodb.js
+++ mongodb.js
@@ -14,5 +14,17 @@ MongoClient.connect(connectionURL,{ useNewUrlParser:true},(error,client)=>{
         }
         console.log(user);
     });
+    db.collection('users').find({ age:30}).toArray((error,users)=>{
+        if(error){
+            return console.log('Unable to find users');
+        }
+        console.log(users);
+    });
+    db.collection('users').find({ age:30}).count((error,count)=>{
+        if(error){
+            return console.log('Unable to find users count');
+        }
+        console.log(count);
+    });
     // console.log('connected correctly');
 });
