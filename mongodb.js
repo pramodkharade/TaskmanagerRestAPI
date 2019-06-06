04eab2faa24c13f6b59f92fd93509f5cab7f6979
pramod.kharade <pramod.kharade@blazeclan.com>
2019-06-06 10:13:44 +0530

    search single record with keys

--- mongodb.js
+++ mongodb.js
@@ -2,40 +2,17 @@ const {MongoClient , ObjectID} = require('mongodb');
 
 const connectionURL = 'mongodb://127.0.0.1:27017';
 const databaseName ='task-manager';
-const id = new ObjectID();
-console.log('ID Is: ',id);
-console.log('TimeStamp: ',id.getTimestamp());
 MongoClient.connect(connectionURL,{ useNewUrlParser:true},(error,client)=>{
     if(error){
         return console.log('Unable to connect database');
     }
     const db = client.db(databaseName);
-    db.collection('users').insertOne({
-        _id:id,
-        name:'Dnyanda Kharade',
-        age:1
+    
+    db.collection('users').findOne({name:'Dnyanda Kharade',age:1},(error,user)=>{
+        if(error){
+            return console.log('Unable to find user');
+        }
+        console.log(user);
     });
-    // db.collection('users')
-    // .insertMany([
-    //     {name:'Shital Bari',age:29}
-    //     ,{name:'Kishor Vitekar',age:30}
-    // ],(error,result)=>{
-    //     if(error){
-    //         return console.log('unable to insert Users..!');
-    //     }
-    // });
-    // db.collection('tasks')
-    // .insertMany([
-    //     {description:'Clanning the house',completed:true},
-    //     {description:'Pot to plants',completed:false},
-    //     {description:'Servicing the Bike',completed:false}
-    // ],(error,result)=>{
-    //     if(error){
-    //         return console.log('unable to insert tasks..!');
-    //     }
-    //     console.log(result.ops);
-    // });
-    
-    
     // console.log('connected correctly');
 });
