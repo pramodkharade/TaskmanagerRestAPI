464d50fc71e7ac02739fd78ed5854baf367bdde7
pramod.kharade <pramod.kharade@blazeclan.com>
2019-06-17 16:22:28 +0530

    async and await integration

--- src/index.js
+++ src/index.js
@@ -6,66 +6,72 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(express.json());
-app.post('/users',(req,res)=>{
+app.post('/users', async(req,res)=>{
     const user = new User(req.body);
-    user.save()
-        .then(()=>{
-            res.status(200).send(user);
-        })
-        .catch((error)=>{
-            res.status(400).send(error);
-        });
+    try {
+        await user.save();
+        res.status(200).send(user);
+    } catch(error){
+        res.status(400).send(error);
+    }
+    // user.save()
+    //     .then(()=>{
+    //         res.status(200).send(user);
+    //     })
+    //     .catch((error)=>{
+    //         res.status(400).send(error);
+    //     });
     
 });
-app.get('/users',(req,res)=>{
-    User.find({}).then(
-        (users)=>{
-            res.status(200).send(users);
-        }
-    ).catch((error)=>{
+app.get('/users',async (req,res)=>{
+    try{
+        const users = await User.find({});
+        res.status(200).send(users);
+    } catch(e){
         res.status(500).send();
-    });
+    }
 });
-app.get('/users/:id',(req,res)=>{
+app.get('/users/:id', async(req,res)=>{
     const _id = req.params;
-    User.findById(_id).then((user)=>{
+    try{
+        const user = await User.findById(_id);
         if(!user){
             return res.status(404).send();
         }
         res.send(user);
-    })
-    .catch((e)=>{
-         res.status(500).send(e);
-    });
-});
-app.post('/tasks',(req,res)=>{
-    const task = new Task(req.body);
-    task.save().then(()=>{
-        res.status(200).send(task);
-    }).catch((error)=>{
-        res.status(400).send(error);
-    });
-});
-app.get('/tasks',(req,res)=>{
-    Task.find({}).then(
-        (tasks)=>{
-            res.status(200).send(tasks);
-        }
-    ).catch((error)=>{
+    }catch(e){
         res.status(500).send();
-    });
+    }
+    
 });
-app.get('/tasks/:id',(req,res)=>{
+app.post('/tasks', async (req,res)=>{
+    const task = new Task(req.body);
+    try{
+        await task.save();
+        res.status(201).send(task);
+    }catch(e){
+        res.status(400).send(e);
+    }
+});
+app.get('/tasks', async(req,res)=>{
+    try{
+        const tasks = await Task.find({});
+        res.status(200).send(tasks);
+    }catch(e){
+        res.status(500).send();
+    }
+});
+app.get('/tasks/:id',async(req,res)=>{
     const _id = req.params;
-    Task.findById(_id).then((task)=>{
+    try{
+        const task = await Task.findById(_id);
         if(!task){
             return res.status(404).send();
         }
         res.send(task);
-    })
-    .catch((e)=>{
-         res.status(500).send(e);
-    });
+    }catch(e){
+        res.status(500).send();
+    }
 });
 
 app.listen(port,()=>{
