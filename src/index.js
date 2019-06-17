a460b4ba27f5d6a4c0532a28cd8d5145d2db263a
pramod.kharade <pramod.kharade@blazeclan.com>
2019-06-17 11:48:20 +0530

    task get by ID

--- src/index.js
+++ src/index.js
@@ -55,6 +55,18 @@ app.get('/tasks',(req,res)=>{
         res.status(500).send();
     });
 });
+app.get('/tasks/:id',(req,res)=>{
+    const _id = req.params;
+    Task.findById(_id).then((task)=>{
+        if(!task){
+            return res.status(404).send();
+        }
+        res.send(task);
+    })
+    .catch((e)=>{
+         res.status(500).send(e);
+    });
+});
 
 app.listen(port,()=>{
     console.log('Server is running on :',port);
