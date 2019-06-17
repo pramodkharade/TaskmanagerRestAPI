c9373e85eeac36f8c3ad836332da330ce4bdcdf4
pramod.kharade <pramod.kharade@blazeclan.com>
2019-06-17 17:42:27 +0530

    update record with patch method and route

--- src/index.js
+++ src/index.js
@@ -44,6 +44,18 @@ app.get('/users/:id', async(req,res)=>{
     }
     
 });
+app.patch('/users/:id', async (req,res)=>{
+    const _id = req.params;
+    try {
+        const user = await User.findByIdAndUpdate(req.params.id,req.body,{new:true,runValidators:true});
+        if(!user){
+            return res.status(404).send();
+        }
+        res.send(user);
+    }catch(e){
+        res.status(400).send(e);
+    }
+});
 app.post('/tasks', async (req,res)=>{
     const task = new Task(req.body);
     try{
