b4d8fcbb9f1d886a3a4ccd9415bd40a53b8df450
pramod.kharade <pramod.kharade@blazeclan.com>
2019-06-18 15:49:46 +0530

    access toekn with jwt while login and save into db

--- src/models/user.js
+++ src/models/user.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 const validator = require('validator');
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 const userSchema = new mongoose.Schema({
     name:{
         type:String,
@@ -38,8 +39,21 @@ const userSchema = new mongoose.Schema({
                 throw new Error('Age must be postive number');
             }
         }
-    }
+    },
+    tokens:[{
+        token:{
+            type:String,
+            require:true
+        }
+    }]
 });
+userSchema.methods.generateAuthToken = async function(){
+    const user = this;
+    const token = jwt.sign({_id:user._id.toString()},'thisisnodejsrestapi');
+    user.tokens = user.tokens.concat({token});
+    await user.save();
+    return token;
+}
  userSchema.statics.findByCredentials = async(email,password)=>{
     const user = await User.findOne({email:email});
     
--- src/routers/user.js
+++ src/routers/user.js
@@ -21,7 +21,8 @@ router.post('/users', async(req,res)=>{
 router.post('/users/login',async(req,res)=>{
     try {
         const user = await User.findByCredentials(req.body.email,req.body.password);
-        res.send(user);
+        const token = await user.generateAuthToken();
+        res.send({user,token});
     }catch(e){
         res.status(404).send();
     }
