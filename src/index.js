dca56eb7368efb57da0f173e3ac2b197f5b792e0
pramod.kharade <pramod.kharade@blazeclan.com>
2019-06-18 09:41:45 +0530

    delete route with user ID

--- src/index.js
+++ src/index.js
@@ -63,6 +63,17 @@ app.patch('/users/:id', async (req,res)=>{
         res.status(400).send(e);
     }
 });
+app.delete('/users/:id',async(req,res)=>{
+    try{
+        const user = await User.findByIdAndDelete(req.params.id);
+        if(!user){
+            res.status(404).send();
+        }
+        res.send(user);
+    }catch(e){
+        res.status(500).send();
+    }
+});
 app.post('/tasks', async (req,res)=>{
     const task = new Task(req.body);
     try{
