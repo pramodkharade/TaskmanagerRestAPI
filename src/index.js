67affc6b85629dad528f9509fa4e09f4e3fc5809
pramod.kharade <pramod.kharade@blazeclan.com>
2019-06-18 16:31:16 +0530

    to disable some methods or put site in maintenance mode

--- src/index.js
+++ src/index.js
@@ -4,11 +4,23 @@ const userRouter = require('./routers/user');
 const taskRouter = require('./routers/task');
 const app = express();
 const port = process.env.PORT || 3000;
+/******To disabled GET request******/
+// app.use((req, res, next) => {
+//     if (req.method == 'GET') {
+//         res.send('GET requests are disabled');
+//     } else {
+//         next();
+//     }
+// });
+/******maintenance Mode******/
+// app.use((req, res, next) => {
+//     res.send('site is currently down. check back soon!');
+// });
 
 app.use(express.json());
 app.use(userRouter);
 app.use(taskRouter);
 
-app.listen(port,()=>{
-    console.log('Server is running on :',port);
+app.listen(port, () => {
+    console.log('Server is running on :', port);
 });
