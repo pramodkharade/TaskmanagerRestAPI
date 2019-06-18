bbfb3b94e4a853bf9ed35a6ddf513cddcf55d396
pramod.kharade <pramod.kharade@blazeclan.com>
2019-06-18 14:53:16 +0530

    login  router with login functionality

--- src/models/user.js
+++ src/models/user.js
@@ -11,6 +11,7 @@ const userSchema = new mongoose.Schema({
         type:String,
         require:true,
         trim:true,
+        unique:true,
         lowercase:true,
         validate(value){
             if(!validator.isEmail(value)){
@@ -39,6 +40,23 @@ const userSchema = new mongoose.Schema({
         }
     }
 });
+ userSchema.statics.findByCredentials = async(email,password)=>{
+    const user = await User.findOne({email:email});
+    
+    if(!user){
+        throw new Error('Unable to login');
+    }
+    const isMatch = await bcrypt.compare(password,user.password);
+    if(!isMatch){
+        throw new Error("Unable to login");
+    }
+    
+    return user;
+}
+/********
+ * Hash the plain text password before saving 
+ * 
+ * ********/
 userSchema.pre('save',async function(next){
     const user = this;
     if(user.isModified('password')){
--- src/routers/user.js
+++ src/routers/user.js
@@ -18,6 +18,14 @@ router.post('/users', async(req,res)=>{
     //     });
     
 });
+router.post('/users/login',async(req,res)=>{
+    try {
+        const user = await User.findByCredentials(req.body.email,req.body.password);
+        res.send(user);
+    }catch(e){
+        res.status(404).send();
+    }
+});
 router.get('/users',async (req,res)=>{
     try{
         const users = await User.find({});
