a44c1bb9a77e47acd32fe4eec07c3ec8e0656cf7
pramod.kharade <pramod.kharade@blazeclan.com>
2019-06-18 09:47:39 +0530

    delete router, delete task by id

--- src/index.js
+++ src/index.js
@@ -122,6 +122,17 @@ app.patch('/tasks/:id', async(req,res)=>{
         res.status(400).send(e);
     }
 });
+app.delete('/tasks/:id',async(req,res)=>{
+    try{
+        const task = await Task.findByIdAndDelete(req.params.id);
+        if(!task){
+            res.status(404).send();
+        }
+        res.send(task);
+    }catch(e){
+        res.status(500).send(e);
+    }
+});
 app.listen(port,()=>{
     console.log('Server is running on :',port);
 });
