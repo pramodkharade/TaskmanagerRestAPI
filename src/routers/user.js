e42cff6cb0aa6da70e2e922a74feca55f7ec9003
pramod.kharade <pramod.kharade@blazeclan.com>
2019-06-20 13:44:41 +0530

    Hide the password and tokens from login response

--- src/routers/user.js
+++ src/routers/user.js
@@ -24,7 +24,7 @@ router.post('/users/login',async(req,res)=>{
     try {
         const user = await User.findByCredentials(req.body.email,req.body.password);
         const token = await user.generateAuthToken();
-        res.send({user,token});
+        res.send({user:user.getPublicProfile(),token});
     }catch(e){
         res.status(404).send();
     }
