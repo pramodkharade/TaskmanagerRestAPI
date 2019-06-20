46868a44ea15ad571caab1e1adb1ac4d3099c041
pramod.kharade <pramod.kharade@blazeclan.com>
2019-06-20 15:17:46 +0530

    create relationship with user and task tables

--- src/routers/task.js
+++ src/routers/task.js
@@ -1,9 +1,14 @@
 const express = require('express');
+const auth = require('../middleware/auth');
 const Task = require('../models/task');
 const router = express.Router();
 
-router.post('/tasks', async (req,res)=>{
-    const task = new Task(req.body);
+router.post('/tasks',auth, async (req,res)=>{
+    //const task = new Task(req.body);
+    const task = new Task({
+        ...req.body,
+        owner:req.user._id
+    });
     try{
         await task.save();
         res.status(201).send(task);
