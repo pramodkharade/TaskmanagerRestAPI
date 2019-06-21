fc1a7663388042cbf0283365df983943d1102183
pramod.kharade <pramod.kharade@blazeclan.com>
2019-06-21 10:40:23 +0530

    timestamps to task and separate the schema

--- src/models/task.js
+++ src/models/task.js
@@ -1,7 +1,6 @@
 const mongoose = require('mongoose');
 const validator = require('validator');
-
-const Task = mongoose.model('Task',{
+const taskSchema = mongoose.Schema({
     description:{
         type:String,
         require:true,
@@ -16,6 +15,10 @@ const Task = mongoose.model('Task',{
         require:true,
         ref:'User'
     }
+},
+{
+    timestamps:true
 });
+const Task = mongoose.model('Task',taskSchema);
 
 module.exports = Task;
