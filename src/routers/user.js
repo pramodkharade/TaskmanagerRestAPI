be4e1d8fd0202af2cd1b6bcd900c2c7fb78b48a6
pramod.kharade <pramod.kharade@blazeclan.com>
2019-06-21 14:46:51 +0530

    file validation like extension and size while file upload

--- src/routers/user.js
+++ src/routers/user.js
@@ -108,7 +108,17 @@ router.delete('/users/me',auth,async(req,res)=>{
     }
 });
 const upload = multer({
-    dest:'avatars'
+    dest:'avatars',
+    limits:{
+        fileSize:1000000
+    },
+    fileFilter(req,file,cb){
+        ///!file.originalname.endsWith('.pdf')
+        if(!file.originalname.match(/\.(png|jpeg|jpg)$/)){
+           return cb(new Error('Please Upload a image '));
+        }
+        cb(undefined,true);
+    }
 });
 router.post('/users/me/avatar',upload.single('avatar'),(req,res)=>{
     res.send('uploaded');
