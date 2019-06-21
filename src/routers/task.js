a7fee11d2495a4d80cdd49654493f9c12f3bd9a7
pramod.kharade <pramod.kharade@blazeclan.com>
2019-06-21 09:38:38 +0530

    delete task with respective owner

--- src/routers/task.js
+++ src/routers/task.js
@@ -62,13 +62,14 @@ router.patch('/tasks/:id',auth, async(req,res)=>{
         res.status(400).send(e);
     }
 });
-router.delete('/tasks/:id',async(req,res)=>{
+router.delete('/tasks/:id',auth,async(req,res)=>{
     try{
-        const task = await Task.findByIdAndDelete(req.params.id);
+        //const task = await Task.findByIdAndDelete(req.params.id);
+        const task = await Task.findOneAndDelete({_id:req.params.id,owner:req.user._id});
         if(!task){
-            res.status(404).send();
+            res.status(404).send(task);
         }
-        res.send(task);
+        res.send();
     }catch(e){
         res.status(500).send(e);
     }
